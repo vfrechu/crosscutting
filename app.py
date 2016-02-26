8f279de3c9b1b273011e77b8bc9e8a6056f76857
vfrechu <vfrechu@gmail.com>
2016-02-26 13:12:04 +0100

    test2

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ import numpy as np
 #%%
 #Import and Prepare Data
 
-df = read_csv('DataCC.csv', index_col=0)
+df = read_csv('DataCC2.csv', index_col=0)
 
 
 # In[6]:
@@ -35,10 +35,9 @@ df = read_csv('DataCC.csv', index_col=0)
 #compute returns
 df_return=df/df.shift(1)-1
 df_return=df_return.ix[1:]
-
 # Retrieve Libor 
 
-tm_USD_libor=0.05
+tm_USD_libor=0.00619
 
 #%%
 #Library of Target Functions
@@ -56,36 +55,81 @@ number_of_months_1=6
 number_of_months_2=3
 
 #Function
-def MSCI_Momentum(df):
-           
-    #compute annualized vol
-    vol=((np.var(df)**0.5)*(len(df)**0.5))
+# INPUTS
+# S&P 100 Prices & Market Cap
+
+# df_mktcap = read_csv('DataCCSP100_mktcap.csv', index_col=0)
+
+# Find the number of scores in the universe
+#Nb_Sec_Universe = len(df_prices.columns)
+# Libor : risk free rate
+ThreeM_USD_libor = 0.00619
+
+# Number of month to compute the momentum
+
+Nb_Month_1 = 7
+Nb_Month_2 = 13
+
+# Z-Score Function:
+# input prices data frame and return scores and 
+# Nb_Month_1 = First period = 3 months for us
+# Nb_Month_2 = Second period = 6 months for us
+
+def Price_Momentum(df,Nb_Month,ThreeM_USD_libor):
     
-    #transform return dataframe
-    df_t=df+1-tm_USD_libor/len(df)
+    Price_Momentum = (df.iloc[-20]/(df.iloc[-Nb_Month*20])) - 1 - ThreeM_USD_libor
+
+    return Price_Momentum
+
+def MSCI_Momentum(df,Nb_Month_1,Nb_Month_2,ThreeM_USD_libor):
+
     
-    #compute momentum for the two specified windows
-    six_m_m=np.prod(df_t.tail(number_of_months_1*len (df)/12))-1
-    six_m_m=np.nan_to_num(six_m_m)
-    three_m_m=np.prod(df_t.tail(number_of_months_2*len (df)/12))-1
-    three_m_m=np.nan_to_num(three_m_m)
+    # Compute returns
+    df_return=df/df.shift(1)-1
+    df_return=df_return.ix[1:]
     
-    #risk constrained momentum
-    rc_six_m_m=(six_m_m/vol).to_frame()
-    rc_six_m_m = rc_six_m_m.iloc[:,0].map(lambda x: 3.0 if x>3.0 else x)
-    rc_six_m_m = rc_six_m_m.map(lambda x: -3.0 if np.abs(x)>3 else x)
-    rc_three_m_m=(three_m_m/vol).to_frame()
-    rc_three_m_m = rc_three_m_m.iloc[:,0].map(lambda x: 3.0 if x>3.0 else x)
-    rc_three_m_m = rc_three_m_m.map(lambda x: -3.0 if np.abs(x)>3 else x)
+    #compute annualized vol from returns
+    vol=((np.var(df_return)**0.5)*(len(df_return)**0.5))
+    
+
+    # Compute momentum using above momentum function
+
+    three_m_m = Price_Momentum(df,Nb_Month_1,ThreeM_USD_libor)
+    six_m_m = Price_Momentum(df,Nb_Month_2,ThreeM_USD_libor)
+
+    # Question Alessandro: Pourquoi using np.nan_to_num instead of .dropna
+    #three_m_m=np.nan_to_num(three_m_m)    
+    #six_m_m=np.nan_to_num(six_m_m)
+    three_m_m=three_m_m.fillna(0)
+    six_m_m=six_m_m.fillna(0)
+
+    
+    # Risk Contrainted to vol and convert to frame
+    rc_three_m_m=(three_m_m/vol)
+    rc_six_m_m=(six_m_m/vol)
+    
+    # COME BACK HERE  : Winderized our RC distributions
+        
+    #rc_six_m_m = rc_six_m_m.iloc[0,:].map(lambda x: 3.0 if x>3.0 else x)
+    #rc_six_m_m = rc_six_m_m.map(lambda x: -3.0 if np.abs(x)>3 else x)
+    
+    #rc_three_m_m = rc_three_m_m.iloc[0,:].map(lambda x: 3.0 if x>3.0 else x)
+    #rc_three_m_m = rc_three_m_m.map(lambda x: -3.0 if np.abs(x)>3 else x)
     
     #calcul of momentum scores
     z_score=rc_six_m_m*0.5+rc_three_m_m*0.5
-    z_df=z_score.to_frame().dropna()
+
+    z_df=z_score.to_frame().fillna(0)
+    
     z_df.columns = ['z_score']
     momentum_z_score_df = z_df.iloc[:,0].map(lambda x: x+1 if x>0 else (1-x)**(-1)).to_frame()
     momentum_z_score_df.columns = ['momentum_z_score']
     
-    return momentum_z_score_df
+    
+    # Ranked Momentum  
+    ranked_momentum_z_score_df=momentum_z_score_df.sort(['momentum_z_score'],ascending=[False])
+    
+    return ranked_momentum_z_score_df
     
 
 #2: Carry Strategies
@@ -111,7 +155,6 @@ def index_vol(df,x):
     #Compute CovMatrix
     Cov_Matrix=np.cov(df, rowvar=0)
     Cov_Matrix=np.nan_to_num(Cov_Matrix)
-    
     return np.dot(np.dot(x,Cov_Matrix),np.transpose(x))
 
 def index_vol_jacobian(df,x):
@@ -137,7 +180,7 @@ target_function=MSCI_Momentum
 # Specify Method of Optimisation
 
 #Method="Ranking"
-Method="Constrained"
+Method="Ranking"
 
 #Specify Additional Parameters (Ranking) 
 nbr_sec=10
@@ -171,15 +214,15 @@ daily_max_var=Max_Vol**2.0/256.0
 #Index Selection Function
 #This is the core of the program
 
-def optimal_weights(df,nbr_sec,Method):
+def optimal_weights(df,nbr_sec,Method,Nb_Month_1,Nb_Month_2,ThreeM_USD_libor):
 
     if Method=="Ranking":    
     
         #compute scores
-        scores=MSCI_Momentum(df)
+        scores=MSCI_Momentum(df,Nb_Month_1,Nb_Month_2,ThreeM_USD_libor)
         
         #rank z scores 
-        scores["Ranking"] = scores['momentum_z_score'].argsort()
+        scores["Ranking"] = (-scores['momentum_z_score']).argsort()
         
         #generate weights
         scores["Weights"] = scores["Ranking"].map(lambda x: 1 if x < nbr_sec else 0)
@@ -192,7 +235,7 @@ def optimal_weights(df,nbr_sec,Method):
         from scipy.optimize import minimize
         
         #Compute Score
-        scores=MSCI_Momentum(df)
+        scores=MSCI_Momentum(df,Nb_Month_1,Nb_Month_2,ThreeM_USD_libor)
         
         #Initiate Weight Array
         Poids_0=np.ones(len(scores))
@@ -221,7 +264,7 @@ def optimal_weights(df,nbr_sec,Method):
         res=minimize(target_fun, Poids_0, #jac = target_fun_derivative,
                    constraints=Constraints, method="SLSQP",
                    bounds=bnds,
-                   options={'disp': True})
+                   options={'disp': True},)
         
         #Extract Weights
         Poids=res.x
@@ -245,40 +288,50 @@ def optimal_weights(df,nbr_sec,Method):
 #t: how many months ago to start the test. 6 Months default
 
 #Note: at the moment the rebalancing frequency is fixed to 20 trading days.
-def back_test(df_return,Method,nbr_sec=6,t=6):
+def back_test(df_return,Method,nbr_sec,t=6):
       
     u=len(df_return)-t*20
     df_return_bt=df_return.ix[:-t*20]
-                      
+    #print df_return_bt.iloc[:-1]
+    #df_return_bt=df_return.ix[5:10]              
         #Compute Optimal Composition
-    Poids_bt=optimal_weights(df_return_bt,nbr_sec,Method)
-                    
+    Poids_bt=optimal_weights(df_return_bt,nbr_sec,Method,Nb_Month_1,Nb_Month_2,ThreeM_USD_libor)
+                  
         #compute returns
     next_returns=df_return.ix[u:].fillna(0)
     next_20_returns=next_returns.head(20)
+
     optimum_return=np.dot(next_20_returns,Poids_bt)
     return_series=Series(optimum_return,index=next_20_returns.index)
     
+    
     for j in range(t-1,0,-1):
             
-        s=len(df_return)-j*20    
+        s=len(df_return)-j*20  
         df_return_bt=df_return.ix[:-j*20]
-                      
+               
         #Compute Optimal Composition
-        Poids_bt=optimal_weights(df_return_bt,nbr_sec,Method)
-                    
+        Poids_bt=optimal_weights(df_return_bt,nbr_sec,Method,Nb_Month_1,Nb_Month_2,ThreeM_USD_libor)
+        print Poids_bt[Poids_bt>0]     
         #compute returns
+
         next_returns=df_return.ix[s:].fillna(0)
         next_20_returns=next_returns.head(20)
+        
+
         optimum_return=np.dot(next_20_returns,Poids_bt)
+
         return_series_prov=Series(optimum_return,index=next_20_returns.index)
         return_series=np.hstack([return_series,return_series_prov])
         
     return_series_date=Series(return_series,index=df_return.ix[u:].index)    
-    
-    base_1_backtest=np.ones(len(return_series_date)+1)
+    true_returns=return_series_date/return_series_date.shift(1)-1
+    #temp solutions need to append the real return
+    true_returns=true_returns.fillna(0.03)
+
+    base_1_backtest=np.ones(len(true_returns)+1)
     for i in range(1,len(base_1_backtest)):
-        base_1_backtest[i]=base_1_backtest[i-1]*(1+return_series_date[i-1])
+        base_1_backtest[i]=base_1_backtest[i-1]*(1+true_returns[i-1])
      
     base_1_backtest_date=Series(base_1_backtest,index=df_return.ix[u-1:].index) 
     
@@ -291,31 +344,25 @@ def back_test(df_return,Method,nbr_sec=6,t=6):
 import matplotlib.pyplot as plt    
 from pylab import rcParams
 
-"""
-
-Sd=back_test(df_return,nbr_sec)
-
-df=df[:-1]
-df_return=df_return[:-1]
 
 
-rcParams['figure.figsize'] = 12, 8
-a=back_test(df_return,nbr_sec)
+
+
+
 df["S&P_100"] = df.sum(axis=1)
-df["S&P_100 Base 1"]=df["S&P_100"]/df["S&P_100"][len(df["S&P_100"])-len(a)]
-b=df["S&P_100 Base 1"].tail(len(a))
+b=df["S&P_100"].tail(len(df))
 sp=b.to_frame()
 sp.columns=["values"]
 sp=sp.reset_index()
 sp=sp['values']+1
 
-"""
+
 
 def dataToJson(a):
     b=a.to_frame()
     b.columns=["values"]
     b=b.reset_index()
-    H=b.drop('Date',1)
+    H=b.drop('index',1)
     d=json.loads(H.to_json(date_format='iso',orient='split'))
     return json.dumps([{"x": date, "y": val} for date, val in zip(d['index'], d['data'])]) 
 
@@ -339,26 +386,14 @@ def page_not_found(e):
 
 @app.route('/',methods=['GET','POST'])
 def intro():
+    
+
     return flask.render_template('index.html')
 
 @app.route('/test/',methods=['GET','POST'])
 def testing():
-    a=back_test(df_return,"Constrained",1)
-    a_return=a/a.shift(1)-1
-    a_return=a_return.to_frame()
-    a_return=a_return.reset_index()
-    a_return=a_return.drop('Date', 1)
-    a_return.columns=["return"]
-    #a_return=a_return.groupby("return").count()
-
-    bins = np.linspace(0, 1, 10)
-    groups = a_return.groupby(np.digitize(a_return, bins)).count()
-    print groups
-    #a=a.hist
-    l=groups.to_json(date_format='iso',orient='split')
-    q=json.loads(l)
-    T=json.dumps([{"label": date, "return": val} for date, val in zip(q['index'], q['data'])])
-    return flask.render_template('test.html',my_data=T)
+    
+    return flask.render_template('test.html')
 
 @app.route('/main/',methods=['GET','POST'])
 def index():
@@ -373,21 +408,36 @@ def index():
     if Method=="Constrained":
         nbr_sec=10
 
-    a = back_test(df_return,Method,nbr_sec)
-
+    a = back_test(df,Method,nbr_sec)
+    des=a.to_frame()
+    des_return=des/des.shift(1)-1
+    des_return.columns=["Description"]
+    des_return=des_return.describe()
     T = dataToJson(a)
- 
-    test_series=optimal_weights(df,nbr_sec,Method)
+    
+    
+
+    test_series=optimal_weights(df,nbr_sec,Method,Nb_Month_1,Nb_Month_2,ThreeM_USD_libor)
 
     test_series=test_series[test_series>0]*100
 
     test_series=test_series.to_frame()
     test_series.columns=["Weights"]
     l=test_series.to_json(date_format='iso',orient='split')
+
     q=json.loads(l)
     weights_graph=json.dumps([{"label": date, "value": val} for date, val in zip(q['index'], q['data'])])
+    weights_bar_chart=json.dumps([{"x": date, "y": val} for date, val in zip(q['index'], q['data'])])
 
-    return flask.render_template('demo.html', my_data=T, pie_data=weights_graph, data=test_series.to_html(classes='weights'))
+
+    spa=sp.tail(121)
+    p=spa.head(1)
+    #print spa.head(1).value()
+    spa=spa/39274.191
+    spjson=dataToJson(spa)
+
+
+    return flask.render_template('demo.html', my_data=T, pie_data=weights_graph, data=test_series.to_html(classes='weights'),df=test_series,bar_data=weights_bar_chart,describe=des_return.to_html(classes='weights'),sp_data=spjson)
 
 
 if __name__ == '__main__':
